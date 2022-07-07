57a31bb3e1592478ba4352f29ac0437ea1a96ed2
jnsgdm <JonasDmoreira@hotmail.com>
2022-07-07 12:34:29 -0300

    serverjs

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const {resolve} = require('path');
+const serveStatic = require('serve-static');
 
-app.use('/',express.static(resolve(__dirname + './dist')));
+app.use(serveStatic(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 3000,(err)=>{
     if(err){
