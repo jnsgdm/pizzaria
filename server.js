7db0e2807d0883b083db2fe0c85f3cf84157e3bf
jnsgdm <JonasDmoreira@hotmail.com>
2022-07-07 12:10:36 -0300

    serverjs

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.use(express.static(__dirname + './dist'));
 
+app.get('/',(req,res)=>{res.sendFile(__dirname+'./dist/html')});
+
 app.listen(process.env.PORT || 3000,(err)=>{
     if(err){
         console.log(err);
