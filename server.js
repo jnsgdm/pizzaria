5103a0e456f126b460412b3bd3ce2c2b97a76278
jnsgdm <JonasDmoreira@hotmail.com>
2022-07-07 12:19:47 -0300

    serverjs

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.use('/',express.static(__dirname + './dist'));
+app.use(express.static(path.join(__dirname + 'dist')));
 
 app.listen(process.env.PORT || 3000,(err)=>{
     if(err){
