dbcb1ab4b46750841aa3bb60fbf293c319519034
jnsgdm <JonasDmoreira@hotmail.com>
2022-07-07 12:03:45 -0300

    serverjs

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use('/',(express.static(__dirname + './dist')));
+
+app.listen(process.env.PORT || 3000,(err)=>{
+    if(err){
+        console.log(err);
+    }
+    console.log('*running*');
+});
